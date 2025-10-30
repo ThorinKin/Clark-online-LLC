963d62bd8c8dfdf2b9cf80a519b58d8f9bb8408d
Soran <DikHardden@gmail.com>
2025-10-30 17:11:25 +0800

    Database debug. -10301711

--- Server/Server/Migrations/20251105120000_add_orders.Designer.cs
+++ Server/Server/Migrations/20251105120000_add_orders.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using MySql.EntityFrameworkCore.Extensions;
+//using MySql.EntityFrameworkCore.Extensions;
 using Server.Models.DataBase;
 
 #nullable disable
--- Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3c4332745712191abd6f11092a4647f78e334db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbe619a9eb10954ebf4bf75c527c0b1683431e85")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
